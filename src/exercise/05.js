da90d463e9ac76f46060ff7a1b91397618de2ad6
jitasek <jitka.moreira@gmail.com>
2022-03-14 16:33:06 +0100

    add exercise solutions

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,19 +4,60 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 🐨 add a className prop to each div and apply the correct class names
+// 🐨 add a className prop to each Box and apply the correct class names
 // based on the text content
 // 💰 Here are the available class names: box, box--large, box--medium, box--small
 // 💰 each of the elements should have the "box" className applied
 
-// 🐨 add a style prop to each div so their background color
+// 🐨 add a style prop to each Box so their background color
 // matches what the text says it should be
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+function Box({className = '', size, style, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+
+const smallBox = (
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
+
+// const myBox = (
+//   <Box
+//     className="box box--small"
+//     style={{backgroundColor: 'lightblue'}}
+//   >
+//     small lightblue box
+//   </Box>
+// )
+
+// const myBox = (
+//   <Box
+//     className={{size: small }
+//     style={{backgroundColor: 'lightblue'}}
+//   >
+//     small lightblue box
+//   </Box>
+// )
 
 function App() {
   return (
@@ -24,6 +65,8 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      {/* {myBox} */}
+      <Box>sizeless box</Box>
     </div>
   )
 }
